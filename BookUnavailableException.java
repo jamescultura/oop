f5fa554accdbb8d093209c1519ae977b16cb44be
jamescultura <james.cultura@normi.edu.ph>
2025-10-18 23:09:04 +0800

    Delete BookUnavailableException.java

--- BookUnavailableException.java
+++
@@ -1,6 +0,0 @@
-// BookUnavailableException.java - Custom Exception
-public class BookUnavailableException extends Exception {
-    public BookUnavailableException(String message) {
-        super(message);
-    }
-}
